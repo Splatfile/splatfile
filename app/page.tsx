fdc11d23b86c8cc535153e70a1ae8f1aa1943928
jelly <chm073@gmail.com>
2024-01-03 20:21:53 +0900

    Add GameCard (WIP)

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { AccountCard } from "@/app/ui/AccountCard";
+import { GameCard } from "@/app/ui/GameCard";
 
 export default function Home() {
   return (
@@ -8,8 +9,18 @@ export default function Home() {
           "h-full w-full max-w-screen-xl rounded-2xl bg-neutral-100 px-2 py-4 sm:p-4"
         }
       >
-        {/* 계정 정보*/}
-        <AccountCard />
+        <div className={"flex flex-col md:flex-row"}>
+          <div className={"flex flex-col"}>
+            {/* 유저 사진 및 프로필 */}
+            <div className={"h-96 w-48 bg-amber-700 text-white"}>
+              대충 유저 사진 및 프로필 영역
+            </div>
+            {/* 인게임 정보 */}
+            <GameCard></GameCard>
+          </div>
+          {/* 계정 정보*/}
+          <AccountCard />
+        </div>
       </main>
     </div>
   );
---
+++ app/ui/GameCard.tsx
@@ -0,0 +1,15 @@
+export function GameCard() {
+  return (
+    <div
+      className={
+        "flex flex-col justify-center gap-2 bg-white px-2 py-12 md:px-24"
+      }
+    >
+      <h2 className={"pb-2 pt-6 text-xl"}>인게임 정보</h2>
+
+      <div className={"flex flex-col"}>
+        <div className={"flex"}></div>
+      </div>
+    </div>
+  );
+}
