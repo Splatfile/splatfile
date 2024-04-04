63bb539f1283612f480af31a1d7e77f59ccaa754
jelly <chm073@gmail.com>
2024-04-05 00:27:48 +0900

    Add Link On Main Page

--- app/(main-views)/page.tsx
+++ app/(main-views)/page.tsx
@@ -1,6 +1,18 @@
+import Link from "next/link";
+
 type PageProps = {};
 
 export default function Page(props: PageProps) {
   // Temporary redirect for development
-  return <div className={"text-xl text-white"}>메인 페이지 만들어야함</div>;
+  return (
+    <div className={"text-xl"}>
+      메인 페이지 만들어야함
+      <Link
+        className={"text-blue-500"}
+        href={"/users/7973f2b0-ee0d-43ab-aeaf-a4d978e1609d/profile"}
+      >
+        임시 프로필로 이동
+      </Link>
+    </div>
+  );
 }
