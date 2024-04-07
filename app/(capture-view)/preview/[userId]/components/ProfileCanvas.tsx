62a1c0a05991a265651f728c3d46b419e276fd81
jelly <chm073@gmail.com>
2024-04-08 00:03:45 +0900

    Update Profile Canvas

--- app/(capture-view)/preview/[userId]/components/ProfileCanvas.tsx
+++ app/(capture-view)/preview/[userId]/components/ProfileCanvas.tsx
@@ -2,36 +2,52 @@
 import { useEffect, useRef } from "react";
 import { loadFonts, renderPlate } from "@/app/plate/lib/render-plate";
 import { useTagStore } from "@/app/plate/lib/store/use-tag-store";
-import { useUserStore } from "@/app/lib/hooks/use-profile-store";
+import {
+  useProfileImageUrl,
+  useUserStore,
+} from "@/app/lib/hooks/use-profile-store";
 
 export function ProfileCanvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const plateRef = useRef<HTMLCanvasElement>(null);
   const tag = useTagStore();
   const userStore = useUserStore();
+  const profileImageUrl = useProfileImageUrl();
 
   const renderImage = () => {
+    if (!profileImageUrl) return;
     const image = new Image();
+    image.src = profileImageUrl;
+
+    image.onload = () => {
+      const canvas = canvasRef.current;
+      if (!canvas) return;
+      const ctx = canvas.getContext("2d");
+      if (!ctx) return;
+
+      ctx.drawImage(image, 20, 20, 267, 400);
+    };
   };
 
   const renderCanvas = () => {
     const canvas = canvasRef.current;
     const plate = plateRef.current;
     if (!canvas || !plate) return;
+    renderPlate(plate, tag);
 
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
 
-    ctx.fillStyle = "#ffffff";
+    ctx.fillStyle = "#bbbbbb";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(plate, 0, 0, 350, 100);
+    renderImage();
+    ctx.drawImage(plate, 20, 420, 267, 76);
   };
 
   useEffect(() => {
     const plate = plateRef.current;
     const canvas = canvasRef.current;
     if (!plate || !canvas) return;
-    renderPlate(plate, tag);
     renderCanvas();
   }, [tag, userStore]);
 
