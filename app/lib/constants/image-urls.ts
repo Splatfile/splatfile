b4be3ae91cf4506e03ab5fcdab9a06b377472b97
jelly <chm073@gmail.com>
2024-04-13 00:08:19 +0900

    Add Logging

--- app/lib/constants/image-urls.ts
+++ app/lib/constants/image-urls.ts
@@ -1,4 +1,3 @@
+export const regularImageUrl = "/ingames/level.png";
 export const rankImageUrl = "/ingames/ranked.png";
 export const salmonImageUrl = "/ingames/salmon.png";
-
-export const regularImageUrl = "/ingames/level.png";
