1bd10b1d436777a0745eb6f15cf3d9a797572671
jelly <chm073@gmail.com>
2024-04-14 02:21:09 +0900

    Fix Webpack Configs

--- app/lib/utils/server-render-plate.ts
+++ app/lib/utils/server-render-plate.ts
@@ -18,12 +18,12 @@ import {
   registerFont,
 } from "canvas";
 import { join } from "path";
-import "./__files/fonts/SplatoonTitle.otf";
-import "./__files/fonts/SplatoonText.otf";
-import "./__files/fonts/KRko/AsiaKERIN-M.otf";
-import "./__files/fonts/KRko/AsiaKCUBE-R.otf";
-import "./__files/fonts/JPja/Kurokane.otf";
-import "./__files/fonts/JPja/Rowdy.otf";
+import "@/app/api/users/[userid]/__files/fonts/SplatoonTitle.otf";
+import "@/app/api/users/[userid]/__files/fonts/SplatoonText.otf";
+import "@/app/api/users/[userid]/__files/fonts/KRko/AsiaKERIN-M.otf";
+import "@/app/api/users/[userid]/__files/fonts/KRko/AsiaKCUBE-R.otf";
+import "@/app/api/users/[userid]/__files/fonts/JPja/Kurokane.otf";
+import "@/app/api/users/[userid]/__files/fonts/JPja/Rowdy.otf";
 
 const bannerSrc = (file: string, custom = false) =>
   `/assets/${custom ? "custom/" : ""}banners/${file}`;
--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(otf)$/i,
+      use: [
+        {
+          loader: "file-loader",
+        },
+      ],
+    });
+    return config;
+  },
   experimental: {
     outputFileTracingIncludes: {
       "api/users/[userid]": ["./app/api/users/[userid]/__files/**/*"],
