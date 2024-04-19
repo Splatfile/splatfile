8f94827ecc92305191c241f3c6e18e96d7d0f7a7
jelly <chm073@gmail.com>
2024-04-19 11:41:30 +0900

    Remove Sleep

--- app/(main-views)/users/[userId]/profile/page.tsx
+++ app/(main-views)/users/[userId]/profile/page.tsx
@@ -56,10 +56,6 @@ export const generateMetadata = async (props: PageProps) => {
   };
 };
 
-const sleep = (ms: number) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
-
 export default async function ProfilePage(props: PageProps) {
   unstable_noStore();
 
