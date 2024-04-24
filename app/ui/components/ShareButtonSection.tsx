61a1070e2cff6f32df64f875c6f554f71fe58455
jelly <chm073@gmail.com>
2024-04-24 10:38:58 +0900

    Add Transition On Share Section

--- app/ui/components/ShareButtonSection.tsx
+++ app/ui/components/ShareButtonSection.tsx
@@ -10,7 +10,7 @@ export function ShareButtonSection(props: ShareButtonSectionProps) {
       <div
         title={"공유하기"}
         className={
-          "flex w-full items-center justify-start gap-4 rounded-md border border-gray-300 bg-white px-4 py-6 drop-shadow-sm"
+          "flex w-full items-center justify-start gap-4 rounded-md border border-gray-300 bg-white px-4 py-6 drop-shadow-sm transition-all hover:-translate-y-0.5 hover:shadow-md "
         }
       >
         <h2 className={"text-xl font-semibold text-neutral-700"}>공유하기</h2>
