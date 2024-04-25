dbc4b5d02507090e93d28735c22e6b05e0939d5b
cake-monotone <cake.monotone@gmail.com>
2024-04-25 21:12:28 +0900

    zlib revert

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
 const { resolve, join } = require("path");
 
-// https://github.com/Automattic/node-canvas/issues/1779#issuecomment-895885846
-// 버셀에서 ZLIB이 오류를 일으키는 문제를 해결하기 위한 코드
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`,
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
-
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   webpack: (config, options) => {
