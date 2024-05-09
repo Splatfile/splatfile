5447a86314352d4a6fb691f7fd091854ddd56e2a
cake-monotone <cake.monotone@gmail.com>
2024-05-09 21:16:38 +0900

    preview 구현

--- app/(capture-view)/preview/[userId]/components/ProfileCanvas.tsx
+++ app/(capture-view)/preview/[userId]/components/ProfileCanvas.tsx
@@ -1,8 +1,12 @@
 "use client";
-import { useCallback, useEffect, useRef } from "react";
+import { RefObject, useCallback, useEffect, useRef, useState } from "react";
 import { loadFonts, renderPlate } from "@/app/plate/lib/render-plate";
 import { useTagStore } from "@/app/plate/lib/store/use-tag-store";
 import QRCode from "qrcode";
+import Konva from "konva";
+import useImage from "use-image";
+import { Stage, Layer, Image as KonvaImage, Text, Rect } from "react-konva";
+
 
 import {
   useGameStore,
@@ -25,12 +29,18 @@ import {
   matchPointRect,
   plateRect,
   profileImageRect,
+  profileImageBorderRadius as profileImageCornerRadius,
   renderMatchPoint,
   renderRankLevelImageAngGetText,
   renderSalmonLevelImageAndGetText,
   renderText,
-  renderWeapons,
+  renderWeapons
 } from "@/app/lib/utils/render-preview-canvas";
+import {
+  rankImageUrl,
+  regularImageUrl,
+  salmonImageUrl,
+} from "@/app/lib/constants/image-urls";
 
 export function ProfileCanvas() {
   const tag = useTagStore();
@@ -62,243 +72,181 @@ export function ProfileCanvasRender({
   gameStore,
   profileImageUrl,
 }: ProfileCanvasRenderProps) {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const stageRef = useRef<Konva.Stage>(null);
   const plateRef = useRef<HTMLCanvasElement>(null);
   const downloadRef = useRef<HTMLAnchorElement>(null);
-  const twitterPreviewRef = useRef<HTMLCanvasElement>(null);
 
-  const renderImage = useCallback(() => {
-    if (!profileImageUrl) return;
-    const image = new Image();
+  const BackgroundImage = () => {
+    const [image] = useImage("/background/body.png");
 
-    // 이미지 CORS를 활성화 함
-    // CORS 허용된 이미지만 사용하도록 제한하는 대신,
-    // canvas 다운로드 시 insecure에러가 발생하지 않음
-    image.crossOrigin = "anonymous";
-    image.src = profileImageUrl;
+    return (
+      <KonvaImage
+        image={image}
+        x={0}
+        y={0}
+        width={canvasWidth}
+        height={canvasHeight}
+        crop={image && {
+          x: 0,
+          y: 0,
+          width: image.width,
+          height: image.width * (canvasHeight / canvasWidth),
+        }}
+        filters={[Konva.Filters.Blur, Konva.Filters.Brighten]}
+        blurRadius={2}
+        brightness={-0.7}
+      />
+    )
+  }
 
-    image.onload = () => {
-      const canvas = canvasRef.current;
-      if (!canvas) return;
-      const ctx = canvas.getContext("2d");
-      if (!ctx) return;
+  const ProfileImage = () => {
+    const [image] = useImage(profileImageUrl ?? "", "anonymous");
 
-      ctx.drawImage(image, ...profileImageRect);
-    };
-  }, [profileImageUrl]);
+    return (
+      <KonvaImage
+        image={image}
+        x={profileImageRect[0]}
+        y={profileImageRect[1]}
+        width={profileImageRect[2]}
+        height={profileImageRect[3]}
+        stroke="white"
+        strokeWidth={4}
+        cornerRadius={profileImageCornerRadius}
+      />
+    )
+  }
 
-  const renderBackground = () => {
-    return new Promise<void>((resolve) => {
-      const image = new Image();
-      image.src = "/background/body.png";
-      image.onload = () => {
-        const canvas = canvasRef.current;
-        if (!canvas) return;
-        const ctx = canvas.getContext("2d");
-        if (!ctx) return;
 
-        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-        resolve();
-      };
-    });
+  const Plate = () => {
+    const [image, setImage] = useState<HTMLImageElement | undefined>(undefined);
+
+    useEffect(() => {
+      const imageObj = new Image();
+
+      const interval = setInterval(async () => {
+        if (!plateRef.current) return;
+        const loaded = await loadFonts();
+        await renderPlate(plateRef.current, tag);
+
+        imageObj.src = plateRef.current.toDataURL();
+
+        if (loaded) clearInterval(interval);
+      }, 3000);
+
+      imageObj.onload = () => {
+        setImage(imageObj);
+      }
+
+      return () => clearInterval(interval);
+    }, [tag]);
+
+    return (
+      <KonvaImage
+        image={image}
+        x={plateRect[0]}
+        y={plateRect[1]}
+        width={plateRect[2]}
+        height={plateRect[3]}
+        stroke={"white"}
+        strokeWidth={4}
+      />
+    )
   };
 
-  const renderCanvas = useCallback(
-    async (
-      tag: ReturnType<typeof useTagStore.getState>,
-      userStore: ReturnType<typeof useUserStore>,
-      gameStore: ReturnType<typeof useGameStore>,
-    ) => {
-      const canvas = canvasRef.current;
-      const plate = plateRef.current;
-      if (!canvas || !plate) return;
-      await renderPlate(plate, tag);
+  const UserInfo = () => {
+    const FONT_SIZE = 24; // tmp
+    const FONT_FAMILY = "Splat-text";
+    return (
+      <>
+        <Text
+          x={358}
+          y={50}
+          text={`이름: ${userStore.switchInfo?.name || userStore.twitterInfo?.name || ""}`}
+          fill="white"
+          fontFamily={FONT_FAMILY}
+          fontSize={FONT_SIZE} />
+        {
+          userStore.switchInfo?.friendCode &&
+          <Text
+            x={662}
+            y={50}
+            text={`친구코드: ${userStore.switchInfo.friendCode}`}
+            fill="white"
+            fontFamily={FONT_FAMILY}
+            fontSize={FONT_SIZE} />
+        }
 
-      const ctx = canvas.getContext("2d");
-      if (!ctx) return;
-      // Background
-      await renderBackground();
-      ctx.globalAlpha = 0.8;
-      ctx.fillStyle = "#222222";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      ctx.globalAlpha = 1;
+        <Rect
+          x={385}
+          y={335}
+          width={615}
+          height={150}
+          cornerRadius={12}
+          fill="#737373"
+          opacity={0.9} />
 
-      // Profile Image
-      renderImage();
-      ctx.drawImage(plate, ...plateRect);
+        <Text
+          x={400}
+          y={353}
+          width={585}
+          height={120}
+          text={userStore.introductionMessage || ""}
+          fill="white"
+          fontFamily={FONT_FAMILY}
+          fontSize={20}
+        />
+      </>
+    )
+  };
 
-      // Left Text Side
-      const nameText = getNameText(
-        userStore.switchInfo?.name || userStore.twitterInfo?.name || "",
-      );
-      const weekendText = getWeekendTimeText(userStore.weekendPlaytime);
-
-      // Left Text Side Rect
-      ctx.strokeStyle = "#dddddd";
-      ctx.lineWidth = 3;
-
-      // const y = nameText.y - topPadding / 2 - topPadding - defaultFontSize;
-      // drawRoundedRect(
-      //   ctx,
-      //   nameText.x - leftPadding / 2,
-      //   y,
-      //   leftSideWidth,
-      //   weekendText.y - y + defaultFontSize,
-      //   12,
-      //   "#bbbbbb",
-      //   0.5,
-      // );
-
-      // Left Text Side Render
-      renderText(ctx, nameText);
-      renderText(
-        ctx,
-        getFriendCodeText(userStore.switchInfo?.friendCode || ""),
-      );
-      renderText(ctx, getPlaytimeTitleText());
-      renderText(ctx, getWeekdayText(userStore.weekdayPlaytime));
-
-      renderText(ctx, weekendText);
-
-      const weaponTitleText = getWeaponTitleText();
-      renderText(ctx, weaponTitleText);
-
-      const weaponKeys = Object.keys(gameStore.weaponGearInfo ?? {}).filter(
-        (w) => gameStore.weaponGearInfo?.[w]?.isActivated,
-      );
-
-      const filteredWeapons = chunkArrayInGroups(
-        weaponKeys,
-        Math.floor(weaponKeys.length / 2),
-      );
-
-      await renderWeapons(
-        ctx,
-        filteredWeapons,
-        getWeaponTitleText().x,
-        getWeaponTitleText().y + 20,
-      );
-
-      // Right Text Side
-
-      const levelTitleText = getLevelTitleText();
-      renderText(ctx, levelTitleText);
-      // renderText(ctx, levelTitleText);
-
-      const regularLevelText = await getRegularLevelText(
-        ctx,
-        levelTitleText,
-        `${gameStore?.level || ""}`,
-      );
-      renderText(ctx, regularLevelText);
-
-      const rankLevelText = await renderRankLevelImageAngGetText(
-        ctx,
-        regularLevelText,
-        `${gameStore?.anarchyBattleRank?.grade || ""}`,
-      );
-      renderText(ctx, rankLevelText);
-
-      const salmonGrade = gameStore?.salmonRunRank?.grade;
-      const salmonLevelText = await renderSalmonLevelImageAndGetText(
-        ctx,
-        rankLevelText,
-        `${salmonGrade ? salmonRunRanksKo[salmonGrade] : ""}`,
-      );
-      renderText(ctx, salmonLevelText);
-
-      if (gameStore.anarchyBattleRank?.grade === "S+") {
-        const rect = matchPointRect(ctx, regularLevelText);
-        renderMatchPoint(ctx, rect, gameStore.xMatchInfo);
-      }
-      const qrUrlRegex =
-        "https://lounge.nintendo.com/friendcode/\\d{4}-\\d{4}-\\d{4}/[A-Za-z0-9]{10}";
-      if (
-        userStore.switchInfo?.friendLink &&
-        userStore.switchInfo.friendLink.match(qrUrlRegex)
-      ) {
-        const qrCode = await QRCode.toCanvas(userStore.switchInfo.friendLink, {
-          margin: 1,
-          color: { light: "#dddddd" },
-        });
-
-        const size = 180;
-
-        ctx.drawImage(
-          qrCode,
-          canvasWidth - size,
-          canvasHeight - size,
-          size,
-          size,
-        );
-      }
-
-      // Preview
-      setTimeout(() => {
-        twitterPreviewRef.current
-          ?.getContext("2d")
-          ?.drawImage(canvas, 0, 0, canvasWidth / 2, canvasHeight / 2);
-      }, 500);
-    },
-    [renderImage],
-  );
-  useEffect(() => {
-    const plate = plateRef.current;
-    const canvas = canvasRef.current;
-    if (!plate || !canvas) return;
-    renderCanvas(tag, userStore, gameStore).then();
-  }, [gameStore, renderCanvas, tag, userStore]);
-
-  useEffect(() => {
-    const plate = plateRef.current;
-
-    if (!plate) return;
-
-    const interval = setInterval(async () => {
-      const loaded = await loadFonts();
-      await renderPlate(plate, tag);
-      await renderCanvas(tag, userStore, gameStore);
-      if (loaded) clearInterval(interval);
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, [gameStore, renderCanvas, tag, userStore]);
+  const GameInfo = () => {
+    return (
+      <>
+        <Rect
+          x={358}
+          y={160}
+          width={620}
+          height={90}
+          cornerRadius={12}
+          fill="#737373"
+          opacity={0.9} />
+      </>
+    )
+  }
 
   const downloadCanvas = () => {
-    const canvas = canvasRef.current;
+    const stage = stageRef.current;
     const link = downloadRef.current;
-    if (!canvas || !link) return;
+    if (!stage || !link) return;
 
     // Canvas 내용을 이미지로 변환
     // 다운로드 링크 설정
 
-    link.href = canvas.toDataURL("image/png");
+    link.href = stage.toDataURL({ mimeType: "image/png" });
     link.click();
   };
 
   return (
     <div className={"p-12 text-white"}>
-      <p>서버 저장용 이미지 </p>
-      <canvas
-        ref={canvasRef}
+      <p>저장용 이미지</p>
+      <canvas ref={plateRef} width={700} height={200} className={"hidden"}></canvas>
+      <Stage
+        ref={stageRef}
         width={canvasWidth}
-        height={canvasHeight}
-      ></canvas>
-      <canvas
-        className={"hidden"}
-        ref={plateRef}
-        width={700}
-        height={200}
-      ></canvas>
+        height={canvasHeight}>
+        <Layer>
+          <BackgroundImage />
+        </Layer>
+        <Layer>
+          <ProfileImage />
+          <Plate />
+        </Layer>
+        <Layer>
+          <UserInfo />
+          <GameInfo />
+        </Layer>
+      </Stage>
 
-      <div className={"p-8"}>
-        <p>트위터에서 실제 보여질 사이즈</p>
-        <canvas
-          ref={twitterPreviewRef}
-          width={canvasWidth / 2}
-          height={canvasHeight / 2}
-        ></canvas>
-      </div>
       <div>
         <a
           ref={downloadRef}
